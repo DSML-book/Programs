dfdad1eb8b08c7c38a44599dbfd333c5abcdeef1
Dirk P. Kroese <kroese@maths.uq.edu.au>
2019-06-05 13:10:07 +1000

    Add files via upload

--- Chapter3/CMCtestloss.py
+++ Chapter3/CMCtestloss.py
@@ -1,8 +1,7 @@
 """ CMCtestloss.py """
-import numpy as np
+import numpy as np, matplotlib.pyplot as plt 
 from numpy.random import rand, randn
 from numpy.linalg import solve
-import matplotlib.pyplot as plt
 
 def generate_data(beta, sig, n):
     u = rand(n, 1)
@@ -15,22 +14,16 @@ sig = 5
 betahat = {}
 plt.figure(figsize=[6,5])
 totMSE = np.zeros(8)
-
 max_p = 8
 p_range = np.arange(1, max_p + 1, 1)
-u, y = generate_data(beta, sig, n)  #training data
 
-    
 for N in range(0,100):
-    
     u, y = generate_data(beta, sig, n)  #training data
     X = np.ones((n, 1))
     for p in p_range:
        if p > 1:
          X = np.hstack((X, u**(p-1)))
-            
        betahat[p] = solve(X.T @ X, X.T @ y)
-    
     
     u_test, y_test = generate_data(beta, sig, n)  #test data
     MSE = []
@@ -40,11 +33,9 @@ for N in range(0,100):
            X_test = np.hstack((X_test, u_test**(p-1)))
        y_hat = X_test @ betahat[p] # predictions
        MSE.append(np.sum((y_test - y_hat)**2/n))
-    
-        
+     
     totMSE = totMSE + np.array(MSE)
     plt.plot(p_range, MSE,'C0',alpha=0.1)
-
 
 plt.plot(p_range,totMSE/N,'r-o')
 plt.xticks(ticks=p_range)
