486603c957bd4ab8755dfdac556f324f4ad7739a
Dirk P. Kroese <kroese@maths.uq.edu.au>
2021-08-02 09:16:33 +1000

    Add files via upload

--- Chapter3/accrejgamma.py
+++ Chapter3/accrejgamma.py
@@ -5,11 +5,11 @@ from numpy.random import rand
 alpha = 1.3
 lam = 5.6
 f = lambda x: lam**alpha * x**(alpha-1) * exp(-lam*x)/gamma(alpha)
-g = lambda x: lam*exp(-lam*x)
+g = lambda x: 4*exp(-4*x)
 C = 1.2
 found = False
 while not found:
-   x = - log(rand())/lam
+   x = - log(rand())/4
    if C*g(x)*rand() <= f(x):
       found = True
 print(x)
